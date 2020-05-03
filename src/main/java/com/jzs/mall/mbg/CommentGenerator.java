33e431aff661b9cdb766f78040646ba42b7c6564
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-05-03 17:46:57 +0800

    Mybatis-Generator

--- src/main/java/com/jzs/mall/mbg/CommentGenerator.java
+++ src/main/java/com/jzs/mall/mbg/CommentGenerator.java
@@ -1,4 +1,37 @@
 package com.jzs.mall.mbg;
 
-public class CommentGenerator {
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.internal.DefaultCommentGenerator;
+import org.mybatis.generator.internal.util.StringUtility;
+
+import java.util.Properties;
+
+public class CommentGenerator extends DefaultCommentGenerator {
+    private boolean addRemarkComments = false;
+
+    @Override
+    public void addConfigurationProperties(Properties properties) {
+        super.addConfigurationProperties(properties);
+        this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    @Override
+    public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
+        String remarks = introspectedColumn.getRemarks();
+        if (addRemarkComments && StringUtility.stringHasValue(remarks)) {
+            addFieldJavaDoc(field, remarks);
+        }
+    }
+
+    private void addFieldJavaDoc(Field field, String remarks) {
+        field.addJavaDocLine("/**");
+        String[] remarkLines = remarks.split(System.getProperty("line.separator"));
+        for (String remarkLine : remarkLines) {
+            field.addJavaDocLine("*" +remarkLine);
+        }
+        addJavadocTag(field, false);
+        field.addJavaDocLine("*/");
+    }
 }
--- src/main/java/com/jzs/mall/mbg/Generator.java
+++ src/main/java/com/jzs/mall/mbg/Generator.java
@@ -1,4 +1,28 @@
 package com.jzs.mall.mbg;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generator {
+    public static void main(String[] args) throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream is = Generator.class.getResourceAsStream("/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration configuration = cp.parseConfiguration(is);
+
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(configuration, callback, warnings);
+
+        myBatisGenerator.generate(null);
+        for (String warning : warnings) {
+            System.out.println(warning);
+        }
+    }
 }
