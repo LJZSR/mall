26f8a44c202d6ae66534eb2fc9f0d6be9a14fe1a
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-05-15 10:57:38 +0800

    整合redis

---
+++ src/main/java/com/jzs/mall/service/RedisService.java
@@ -0,0 +1,19 @@
+package com.jzs.mall.service;
+
+
+/**
+ * redis操作service
+ * Created by jzs on 2020/5/15
+ */
+public interface RedisService {
+
+    void set(String key, String value);
+
+    String get(String key);
+
+    boolean expire(String key, long expire);
+
+    void remove(String key);
+
+    Long increment(String key, long delta);
+}
