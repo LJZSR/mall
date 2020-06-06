dc9f36283b8cc67c0c5641e595e0d62105be40c5
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-06-06 22:27:57 +0800

    Service实现类

--- src/main/java/com/jzs/mall/service/impl/UmsAdminServiceImpl.java
+++ src/main/java/com/jzs/mall/service/impl/UmsAdminServiceImpl.java
@@ -1,16 +1,27 @@
 package com.jzs.mall.service.impl;
 
 import com.jzs.mall.common.utils.JwtTokenUtil;
+import com.jzs.mall.dao.UmsAdminRoleRelationDao;
+import com.jzs.mall.mbg.mapper.UmsAdminMapper;
+import com.jzs.mall.mbg.mapper.UmsPermissionMapper;
 import com.jzs.mall.mbg.model.UmsAdmin;
+import com.jzs.mall.mbg.model.UmsAdminExample;
 import com.jzs.mall.mbg.model.UmsPermission;
 import com.jzs.mall.service.UmsAdminService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,23 +39,68 @@ public class UmsAdminServiceImpl implements UmsAdminService {
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${jwt.tokenHead}")
+    private String tokenHead;
+
+    @Autowired
+    private UmsAdminMapper adminMapper;
+
+    @Autowired
+    private UmsAdminRoleRelationDao adminRoleRelationDao;
+
+
     @Override
     public UmsAdmin getUmsAdminByUsername(String username) {
+        UmsAdminExample example = new UmsAdminExample();
+        example.createCriteria().andUsernameEqualTo(username);
+        List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+        if (adminList != null && adminList.size() > 0) {
+            return adminList.get(0);
+        }
         return null;
     }
 
     @Override
     public UmsAdmin register(UmsAdmin umsAdminParam) {
-        return null;
+        UmsAdmin umsAdmin = new UmsAdmin();
+        BeanUtils.copyProperties(umsAdminParam, umsAdmin);
+        umsAdmin.setCreateTime(new Date());
+        umsAdmin.setStatus(1);
+        //查询是否有用户名相同的用户
+        UmsAdminExample example = new UmsAdminExample();
+        example.createCriteria().andUsernameEqualTo(umsAdmin.getUsername());
+        List<UmsAdmin> adminList = adminMapper.selectByExample(example);
+        if (adminList.size() > 0) {
+            return null;
+        }
+        String encodePassword = passwordEncoder.encode(umsAdmin.getPassword());
+        umsAdmin.setPassword(encodePassword);
+        adminMapper.insert(umsAdmin);
+        return umsAdmin;
     }
 
     @Override
     public String login(String username, String password) {
-        return null;
+        String token = null;
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+                throw new BadCredentialsException("密码不正确");
+            }
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            token = jwtTokenUtil.generateToken(userDetails);
+        } catch (Exception e) {
+            LOGGER.warn("登陆异常:{}", e.getMessage());
+        }
+        return token;
     }
 
     @Override
     public List<UmsPermission> getPermissionList(Long adminId) {
-        return null;
+        return adminRoleRelationDao.getPermissionList(adminId);
     }
 }
