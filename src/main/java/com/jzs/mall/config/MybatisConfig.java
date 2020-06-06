03bdfd56203202857c499b1b390476de2c089254
LJZSR <43992884+LJZSR@users.noreply.github.com>
2020-06-06 16:31:06 +0800

    package-scan

--- src/main/java/com/jzs/mall/config/MybatisConfig.java
+++ src/main/java/com/jzs/mall/config/MybatisConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("com.jzs.mall.mbg.mapper")
+@MapperScan({"com.jzs.mall.mbg.mapper", "com.jzs.mall.dao"})
 public class MybatisConfig {
 
 }
